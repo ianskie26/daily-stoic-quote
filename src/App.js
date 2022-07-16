523ad05c7db7f48aff02b81c7459c52be9124d6d
Ian <ian.luciano073@gmail.com>
2022-07-16 12:12:06 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
+
+  const [advice, setAdvice] = React.useState({})
+
+  React.useEffect(() => {
+    fetch("https://stoicquotesapi.com/v1/api/quotes/random")
+      .then(res => res.json())
+      .then(data => setAdvice(data))
+      .catch(err => console.error(err));
+  },[])
+
+  console.log(advice)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="center--wrapper">
+      <div className="box">
+        <h2 className="item">{advice.body}</h2>
+        <p className="item">{advice.author}</p>
+      </div>
+  </div>
+  )
 }
-
-export default App;
